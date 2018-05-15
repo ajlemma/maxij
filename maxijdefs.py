a43d5ff74f2c0a103f903083ec5605dc15398e3b
amanda <ajtownsend@astro.ufl.edu>
2018-05-14 23:17:20 -0400

    Removed bad pixel list dependency Updated fits reading to use fits.getdata Deleted rd_shifts function (code uses pandas dataframes now)

--- maxijdefs.py
+++ maxijdefs.py
@@ -22,55 +22,18 @@ def fix_bad_pixels(im0, badpix_list):
         im0[pix] = 0.0
     return im0
 
-def stack_maxi(pathname, n1, n2, badpix_list):
+def stack_maxi(pathname, n1, n2):
     # given a pathnam, stacks the images from n1 to n2 in the index
     maxi_files = get_filelist_maxi(pathname)
-
     n = n2 - n1  # number to stack
-    nct = 0
-    ict = 0
 
     for i in range(n1, n2):
         fnam = pathname + maxi_files[i]  # make the full filename
-        # print fnam
-        fID = fits.open(fnam)
-        im0 = fID[0].data
-        # print im0.shape
-        im1 = fix_bad_pixels(im0, badpix_list)
-        fID.close()
+        im0 = fits.getdata(fnam)
+
         if i == n1:  # first one
-            imf = np.zeros(im1.shape)
-        imf = imf + im1
+            imf = np.zeros(im0.shape)
+        imf = imf + im0
 
     return imf / n
 
-# def rd_shifts_maxi(fnam):
-#     # reads the shifts file
-#
-#     f = open(fnam, "r")
-#     filenams = list()
-#     xlist = list()
-#     ylist = list()
-#     clist = list()
-#
-#     while True:
-#         line = f.readline()
-#
-#         if not line: break
-#         jnk = line.split()
-#
-#         if len(jnk) == 4:
-#             filenams.append(jnk[0])
-#             xlist.append(float(jnk[2]))
-#             ylist.append(float(jnk[1]))
-#             clist.append(float(jnk[3]))
-#
-#         if len(jnk) == 5:
-#             filenams.append(jnk[1])
-#             xlist.append(float(jnk[3]))
-#             ylist.append(float(jnk[2]))
-#             clist.append(float(jnk[4]))
-#
-#     f.close()
-#
-#     return filenams, np.array(ylist), np.array(xlist), np.array(clist)
