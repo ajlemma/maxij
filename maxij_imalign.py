371ebb159199bf69db741e80f8c543173c01ebd1
amanda <ajtownsend@astro.ufl.edu>
2018-05-15 15:33:07 -0400

    Added multiprocessing functionality

--- maxij_imalign.py
+++ maxij_imalign.py
@@ -11,14 +11,14 @@ by A Townsend
 
 from maxijdefs import *
 import pandas as pd
-import numpy as np
+# import numpy as np
 # import matplotlib.pyplot as plt
 # from scipy.signal import fftconvolve
 from scipy.ndimage.interpolation import shift
 from astropy.io import fits
 import time
 from datetime import datetime
-# from multiprocessing import Pool
+from multiprocessing import Pool
 
 #######
 
@@ -29,27 +29,43 @@ scipathnam = pathnam + 'science/' #folder with science images
 apathnam = pathnam + 'aligned/' #folder for aligned images
 time0 = time.time()
 print "start: " + str(datetime.now())
+print "Processing images for " + night
 
+print "getting list of filenames from " + night + "/science ..."
 fnames = get_filelist_maxi(scipathnam)  # list of sorted filenames for sci images in pathnam
+
+print "getting image shift data from " + night + '/imshifts_' + night + ".txt ..."
 imshifts = pd.read_csv(pathnam + 'imshifts_' + night + '.txt',
                          header=None,
                          names = ["fname", "y", "x", "ampcorr"],
                          index_col='fname',
                          delimiter=r"\s+")
 
-# for img in fnames:
 
-i = 0
-im0 = fits.getdata(scipathnam+fnames[i])
+## first and last images to stack, and total no. of images
+n1 = 0                          # first
+n2 = len(fnames)                # last
+n_tot = n2 - n1                 # total
 
-im1 = shift(im0, (imshifts.loc[fnames[i]]["y"], imshifts.loc[fnames[i]]["x"]), mode='constant', cval=0.0)
-im2 = rebin(im1, (im1.shape[0] / 4, im1.shape[1] / 4))
-write_to_fits(apathnam + fnames[i].split('.')[0]+'_aligned.' + fnames[i].split('.')[1], im2)  # write to aligned subdir, add suffix "_aligned"
+## Parallelized version:
+def align_rebin(filename):
+    # i = 0
+    # print filename
+    im0 = fits.getdata(scipathnam+filename)
+    im1 = shift(im0, (imshifts.loc[filename]["y"], imshifts.loc[filename]["x"]), mode='constant', cval=0.0)
+    im2 = rebin(im1, (im1.shape[0] / 4, im1.shape[1] / 4))
+    write_to_fits(apathnam + filename.split('.')[0]+'_aligned.' + filename.split('.')[1], im2)  # write to aligned subdir, add suffix "_aligned"
+    return
 
-# next: turn above "for loop" into multiprocessing fns
+p = Pool(7)
+print "shifting images..."
+# for img in fnames[n1:n2]:
+#     align_rebin(img)
+# p.imap_unordered(align_rebin,fnames[n1:n2],10)
 
+p.map(align_rebin,fnames[n1:n2],10)
 
-# print ajt_shifts.loc["maxij_1s_0027_03-25-31_reduced.fits"]["x"] ## how to use indexes in pd df
+print "Aligned & rebinned images are saved at " + night + '/aligned with the suffix "_aligned"'
 
 
 time1 = time.time()
