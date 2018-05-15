8fa5db68a943dcc23f16fa9b8019cdb50b70a51b
amanda <ajtownsend@astro.ufl.edu>
2018-05-15 00:07:31 -0400

    Added image shift&rebin functionality; ready to be parallelized

--- maxij_imalign.py
+++ maxij_imalign.py
@@ -11,8 +11,10 @@ by A Townsend
 
 from maxijdefs import *
 import pandas as pd
+import numpy as np
 # import matplotlib.pyplot as plt
 # from scipy.signal import fftconvolve
+from scipy.ndimage.interpolation import shift
 from astropy.io import fits
 import time
 from datetime import datetime
@@ -29,30 +31,25 @@ time0 = time.time()
 print "start: " + str(datetime.now())
 
 fnames = get_filelist_maxi(scipathnam)  # list of sorted filenames for sci images in pathnam
-ajt_shifts = pd.read_csv(pathnam + 'imshifts_' + night + '.txt',
+imshifts = pd.read_csv(pathnam + 'imshifts_' + night + '.txt',
                          header=None,
                          names = ["fname", "y", "x", "ampcorr"],
                          index_col='fname',
                          delimiter=r"\s+")
-## to compare to steve's shifts (tbd)
-# sse_shifts = pd.read_csv('/home/amanda/Desktop/maxij_photanalysis_2018-05-05/shifts328.txt',
-#                          header=None,
-#                          names=["iter", "fname", "y", "x", "ampcorr"],
-#                          index_col='fname',
-#                          delimiter=r"\s+")
-#
-# plt.plot(ajt_shifts.loc[fnames]["y"],sse_shifts.loc[fnames]["y"],'.')
-# plt.show()
 
-for img in fnames:
-    im0 = fits.getdata(scipathnam+fname)
+# for img in fnames:
 
+i = 0
+im0 = fits.getdata(scipathnam+fnames[i])
+
+im1 = shift(im0, (imshifts.loc[fnames[i]]["y"], imshifts.loc[fnames[i]]["x"]), mode='constant', cval=0.0)
+im2 = rebin(im1, (im1.shape[0] / 4, im1.shape[1] / 4))
+write_to_fits(apathnam + fnames[i].split('.')[0]+'_aligned.' + fnames[i].split('.')[1], im2)  # write to aligned subdir, add suffix "_aligned"
+
+# next: turn above "for loop" into multiprocessing fns
 
 
-
-
-
-# print ajt_shifts.loc["maxij_1s_0027_03-25-31_reduced.fits"]["x"]
+# print ajt_shifts.loc["maxij_1s_0027_03-25-31_reduced.fits"]["x"] ## how to use indexes in pd df
 
 
 time1 = time.time()
