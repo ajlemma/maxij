274c9b3b61dacbf3cfc736f3532119c067285ab6
amanda <ajtownsend@astro.ufl.edu>
2018-05-15 22:05:19 -0400

    Added get_sec and parse_time functions

--- maxijdefs.py
+++ maxijdefs.py
@@ -40,3 +40,11 @@ def stack_maxi(pathname, n1, n2):
 def rebin(a, shape):
     sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
     return a.reshape(sh).mean(-1).mean(1)
+
+def get_sec(time_str):
+    h, m, s = time_str.split('-')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+def parse_time(fname):
+    ss = fname.find('-')
+    return get_sec(fname[ss - 2:ss + 6])
