0bde701ee5bed9e26b065e1c9db2b86691cbd67d
amanda <ajtownsend@astro.ufl.edu>
2018-05-16 22:53:39 -0400

    added database stuff and then moved it to maxij_database_init instead this program doesn't do much of anything yet

--- maxij_multiphot.py
+++ maxij_multiphot.py
@@ -7,6 +7,7 @@ by A Townsend
 
 from maxijdefs import *
 import pandas as pd
+from natsort import natsorted
 import time
 from datetime import datetime
 from multiprocessing import Pool
@@ -30,6 +31,7 @@ fnames = get_filelist_maxi(apathnam)  # list of sorted filenames for aligned ima
 print "getting list of reference star positions..."
 refstars = pd.read_csv(pathnam + 'ref_stars_xy.txt', delimiter=r"\s+")
 
+
 print "getting os timestamps..."
 ostime = pd.read_csv(pathnam + 'os_timestamps.txt',
                      skiprows=2,
@@ -38,10 +40,47 @@ ostime = pd.read_csv(pathnam + 'os_timestamps.txt',
                      delimiter=r"\s+")
 # print len(ostime)
 ostime = ostime[ostime['filename1'].str.contains('maxij_1s')]
-# print ostime['filename1'][0],ostime['filename2'][0]
-for n in ostime['filename1']:
-    print n.split('_')[-1]
+fileID_os = [n.split('_')[-1] for n in ostime['filename1']]
+ostime['fileID'] = fileID_os
+ostime = ostime.set_index('fileID')
 
+fileID_os = natsorted(fileID_os)
+
+## shifted database to it's own code (maxi_database_init) so read that in here instead & then do photometry
+# print "initializing database..."
+# ## set up a pandas dataframe for collection of data to output
+# fileID = [n.split('_')[2] for n in fnames]
+# maxidat = {'fileID': fileID,
+#            'filename': fnames,
+#
+#            'filetime': np.nan,
+#            'filetime_s': np.nan,
+#            'os_time': np.nan,
+#
+#            'shift_x': np.nan,
+#            'shift_y': np.nan,
+#            'shift_corr_amplitude': np.nan,
+#
+#            'gauss_params_0_amplitude': np.nan,
+#            'gauss_params_1_x0': np.nan,
+#            'gauss_params_2_y0': np.nan,
+#            'gauss_params_3_sigma_x': np.nan,
+#            'gauss_params_4_sigma_y': np.nan,
+#            'gauss_params_5_theta': np.nan,
+#            'gauss_params_6_offset': np.nan,
+#            'gauss_sigma_avg': np.nan,   #avg of sigma_x and sigma_y
+#
+#            'phot_tyc': np.nan,
+#            'phot_maxij': np.nan,
+#            'phot_ref2': np.nan,
+#            'phot_ref3': np.nan,
+#            'phot_ref4': np.nan,
+#            'phot_ref6': np.nan
+#            }
+# maxiframe = pd.DataFrame(maxidat,index=fileID)
+#
+#
+# print maxiframe
 
 # ## first and last images to photometer, and total no. of images
 # n1 = 0                          # first
