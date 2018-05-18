3348b1add86667bc1e6418697fe36ae5dfb77439
amanda <ajtownsend@astro.ufl.edu>
2018-05-18 14:00:26 -0400

    added savealigned for current data

--- maxij1820_main.py
+++ maxij1820_main.py
@@ -1,21 +1,25 @@
 #####
-# from maxij_makeref import *
+from maxij_makeref import *
 from maxij_getshifts import *
+from maxij_savealigned import *
 #####
 
 night = '2018-03-28'
 ## makeref(night) #done
 ## getshifts(night) #done
+## savealigned(night) #done
 
 night = '2018-03-29'
 ## makeref(night,330,350) #done
-getshifts(night)
+## getshifts(night) #done
+savealigned(night)
 
 night = '2018-03-30'
 ## makeref(night,6010,6030) #done
-getshifts(night)
+## getshifts(night) done
+savealigned(night)
 
 night = '2018-05-05'
 ## makeref(night,1000, 1015) #done
 ## getshifts(night) #done
-
+savealigned(night)
