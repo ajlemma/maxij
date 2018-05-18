5ff2a5dc322681b45861ac48c07f2ae70fb51354
amanda <ajtownsend@astro.ufl.edu>
2018-05-18 02:03:33 -0400

    master to run all function-versions of programs for any/all nights

--- maxij1820_main.py
+++ maxij1820_main.py
@@ -1,16 +1,21 @@
-from maxij_makeref import *
+#####
+# from maxij_makeref import *
 from maxij_getshifts import *
+#####
+
+night = '2018-03-28'
+# makeref(night)
+# getshifts(night)
+
+night = '2018-03-29'
+# makeref(night,330,350)
+getshifts(night)
+
+night = '2018-03-30'
+# makeref(night,6010,6030)
+getshifts(night)
 
 night = '2018-05-05'
-# night_path = '/media/amanda/demeter/maxi_j1820_070/'
+# makeref(night,1000, 1015)
+# getshifts(night)
 
-#demeter is the default but should be changed for other computers, this is the folder with the night folder in it.
-#night folder should contain flat-fielded & dark subtracted images in a folder called "science"
-# as well as masters in a folder called "masters" and an os timestamps file
-
-# make a refence image for correlation & finding star positions
-# reference gets saved as ref_stack.fits in night directory
-makeref(night,1000, 1015)
-
-# calculates shifts via cross-correlation for each image wrt to the reference image created above
-getshifts(night)
