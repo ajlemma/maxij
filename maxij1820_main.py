0508bfda5fe542ee2141c9f45be890a1e067390d
amanda <ajtownsend@astro.ufl.edu>
2018-05-18 02:46:07 -0400

    added start/finish time functions

--- maxij1820_main.py
+++ maxij1820_main.py
@@ -4,18 +4,18 @@ from maxij_getshifts import *
 #####
 
 night = '2018-03-28'
-# makeref(night)
-# getshifts(night)
+## makeref(night) #done
+## getshifts(night) #done
 
 night = '2018-03-29'
-# makeref(night,330,350)
+## makeref(night,330,350) #done
 getshifts(night)
 
 night = '2018-03-30'
-# makeref(night,6010,6030)
+## makeref(night,6010,6030) #done
 getshifts(night)
 
 night = '2018-05-05'
-# makeref(night,1000, 1015)
-# getshifts(night)
+## makeref(night,1000, 1015) #done
+## getshifts(night) #done
 
