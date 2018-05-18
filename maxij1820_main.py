6997e16d0d3cdfd47395f540f564aba16c6099e6
amanda <ajtownsend@astro.ufl.edu>
2018-05-18 17:47:48 -0400

    commented out completed steps for 3/29, 3/30, 5/05

--- maxij1820_main.py
+++ maxij1820_main.py
@@ -12,14 +12,14 @@ night = '2018-03-28'
 night = '2018-03-29'
 ## makeref(night,330,350) #done
 ## getshifts(night) #done
-savealigned(night)
+## savealigned(night) #done
 
 night = '2018-03-30'
 ## makeref(night,6010,6030) #done
 ## getshifts(night) done
-savealigned(night)
+## savealigned(night) #done
 
 night = '2018-05-05'
 ## makeref(night,1000, 1015) #done
 ## getshifts(night) #done
-savealigned(night)
+## savealigned(night) #done
