03bc1e88984970af70e731c336dd831ff5aac384
amanda <ajtownsend@astro.ufl.edu>
2018-05-23 14:58:45 -0400

    added initdb to current set of nights

--- maxij1820_main.py
+++ maxij1820_main.py
@@ -2,24 +2,29 @@
 from maxij_makeref import *
 from maxij_getshifts import *
 from maxij_savealigned import *
+from maxij_initdb import *
 #####
 
 night = '2018-03-28'
 ## makeref(night) #done
 ## getshifts(night) #done
 ## savealigned(night) #done
+# initdb(night)
 
 night = '2018-03-29'
 ## makeref(night,330,350) #done
 ## getshifts(night) #done
 ## savealigned(night) #done
+# initdb(night)
 
 night = '2018-03-30'
 ## makeref(night,6010,6030) #done
 ## getshifts(night) done
 ## savealigned(night) #done
+# initdb(night)
 
 night = '2018-05-05'
 ## makeref(night,1000, 1015) #done
 ## getshifts(night) #done
 ## savealigned(night) #done
+# initdb(night)
