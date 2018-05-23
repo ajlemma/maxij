b9591eeb3cc8d7bbfee06074e29312658bd63079
amanda <ajtownsend@astro.ufl.edu>
2018-05-23 19:43:11 -0400

    imports photometry code (not ready to run photometry code yet though)

--- maxij1820_main.py
+++ maxij1820_main.py
@@ -3,28 +3,35 @@ from maxij_makeref import *
 from maxij_getshifts import *
 from maxij_savealigned import *
 from maxij_initdb import *
+from maxij_dophot import *
 #####
+
 
 night = '2018-03-28'
 ## makeref(night) #done
 ## getshifts(night) #done
 ## savealigned(night) #done
 # initdb(night)
+# dophot(night)
 
 night = '2018-03-29'
 ## makeref(night,330,350) #done
 ## getshifts(night) #done
 ## savealigned(night) #done
 # initdb(night)
+# dophot(night)
+
 
 night = '2018-03-30'
 ## makeref(night,6010,6030) #done
 ## getshifts(night) done
 ## savealigned(night) #done
 # initdb(night)
+# dophot(night)
 
 night = '2018-05-05'
 ## makeref(night,1000, 1015) #done
 ## getshifts(night) #done
 ## savealigned(night) #done
 # initdb(night)
+# dophot(night)
