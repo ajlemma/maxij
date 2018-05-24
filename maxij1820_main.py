e708021eba76c2b779a36544dee1c9f1e1923464
amanda <ajtownsend@astro.ufl.edu>
2018-05-24 16:07:13 -0400

    updated with troubleshooting notes

--- maxij1820_main.py
+++ maxij1820_main.py
@@ -15,20 +15,24 @@ night = '2018-03-28'
 # dophot(night)
 
 
-night = '2018-03-29'
-# ## makeref(night,330,350) #done
-# ## getshifts(night) #done
-# ## savealigned(night) #done
-# initdb(night)
-# dophot(night)
+night = '2018-03-29' # 5953 images
+# ## makeref(night,330,350) #redone with aij & more images
+# getshifts(night) # done; ~ 16 min
+# savealigned(night) # done; ~ 11.5 min
+# initdb(night) # done; ~ 11 seconds
+# dophot(night) # done; ~ 1 min
 
 
-night = '2018-03-30'
-# ## makeref(night,6010,6030) #done
-# ## getshifts(night) done
-# ## savealigned(night) #done
-# initdb(night)
-# dophot(night)
+night = '2018-03-30' #9668 images -- deleted a few b/c super bad weather meant bad shifts.
+# ## makeref(night,6010,6030) #redone with aij & more images
+# getshifts(night) # done; ~ 27 min !! need to redo this one cuz i fucked it up;
+## but the aligned images are still saved so it should be okay
+# savealigned(night) # done; ~ 23 min
+## can make a ref_stars_xy.txt file after you've got the first few aligned images saved
+## which you need for dophot (done)
+initdb(night) # done; ~ 25 s
+dophot(night) # done; ~ 2 min -- need to not kill process for not calculating gaussian
+# also consider switching phot to grab fileIDs from images in the folder instead of fIDs in the df?
 
 
 night = '2018-05-05'
@@ -36,4 +40,4 @@ night = '2018-05-05'
 # ## getshifts(night) #done
 # ## savealigned(night) #done
 # initdb(night)
-dophot(night)
+# dophot(night)
