c532874019fe60ddb01efc37aefd986fe6ee960f
amanda <ajtownsend@astro.ufl.edu>
2018-06-04 20:44:20 -0400

    initial upload

---
+++ add_shiftdata_to_df.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+# import numpy as np
+
+
+# path='/media/amanda/demeter/maxi_j1820_070/'
+# night = '2018-05-05'
+
+path = '/home/amanda/PycharmProjects/maxij/'
+night = "test"
+
+#read in data as pandas dataframe:
+data = pd.read_pickle(path+night+'/photdata_'+night+'.pkl')
+
+
+shiftdata = pd.read_csv(path+night+'/imshifts_test.txt',
+                        header = None,
+                        delimiter = r"\s+",
+                        names=['filename', 'xshift', 'yshift','shift_amplitude'],)
+
+print shiftdata
