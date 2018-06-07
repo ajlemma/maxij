40d4e003a7dc9b565cf99a613c7d573b1a91f647
amanda <ajtownsend@astro.ufl.edu>
2018-06-07 13:53:37 -0400

    added code to assign values; still needs file IDs in shift frame

--- add_shiftdata_to_df.py
+++ add_shiftdata_to_df.py
@@ -17,7 +17,25 @@ shiftdata = pd.read_csv(path+night+'/imshifts_test.txt',
                         header = None,
                         delimiter = r"\s+",
                         names=['filename', 'xshift', 'yshift','shift_amplitude'],)
-
+fileID = [n.split('_')[2] for n in fnames]
 # use maxij_initdb code to put shifts values into big db and then resave them
 
-print shiftdata
+fileID = data['fileID']
+
+print fileID
+
+for results in p.imap_unordered(partial(assignshifts,
+                                        shiftdata=shiftdata),
+                                fileID, 25):
+
+    fid = results[0]
+    data.loc[fid, 'shift_x'] = results[1]
+    data.loc[fid, 'shift_y'] = results[2]
+    data.loc[fid, 'shift_corr_amplitude'] = results[3]
+
+
+def assignshifts(fileID,shiftdata):
+    shift_x = shiftdata.iloc[fileID,'xshift']
+    shift_y = shiftdata.loc[fileID,'yshift']
+
+    return [fileID,shift_x,shift_y,shift_corr_amplitude]
