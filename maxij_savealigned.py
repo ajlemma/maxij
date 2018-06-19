b4d2d4333a68d85f7c5e8312b4ae107ada53249b
amanda <ajtownsend@astro.ufl.edu>
2018-06-18 22:42:00 -0400

    updated for new time fns, added checkfiles, uses df for shift data & filenames, and saves printed output to logfile. shifts are not being correctly found atm, though.

--- maxij_savealigned.py
+++ maxij_savealigned.py
@@ -1,11 +1,12 @@
 #######
 ''' maxij_savealigned.py
 by A Townsend
-1. read in imshifts file created by maxij_getshifts
-2. open each image
-3. shift image
-4. rebin image
-5. save new image in aligned folder
+- check files
+- read in pandas dataframe with shift data & filenames
+- open each image
+- shift image
+- rebin image
+- save new image in aligned folder
 '''
 #######
 
@@ -15,48 +16,88 @@ from scipy.ndimage.interpolation import shift
 from astropy.io import fits
 from multiprocessing import Pool
 from functools import partial
+import os
+
 #######
 
 def savealigned(night, n1 = 0, n2 = 'max', path='/media/amanda/demeter/maxi_j1820_070/'):
 
-    time0 = timestart()
-    print "Processing images for " + night
+    loglist = []  # initialize log
+
+    # housekeeping stuff
+    msg = "Running 'maxij_savealigned.py' code on data in '" + night + "' folder..."
+    loglist = addlog(msg, loglist)
+
+    time0, msg = timestart()
+    loglist = addlog(msg, loglist)
 
     pathnam = path+night+'/'
     scipathnam = pathnam + 'science/'  # folder with science images
     apathnam = pathnam + 'aligned/'  # folder for aligned images
 
-    print "getting list of filenames from " + night + "/science ..."
-    fnames = get_filelist_maxi(scipathnam)  # list of sorted filenames for sci images in pathnam
-    print "total science frames:  " + str(len(fnames))
 
-    print "getting image shift data from " + night + '/imshifts_' + night + ".txt ..."
-    imshifts = pd.read_csv(pathnam + 'imshifts_' + night + '.txt',
-                             header=None,
-                             names = ["fname", "y", "x", "ampcorr"],
-                             index_col='fname',
-                             delimiter=r"\s+")
+    # check to make sure all necessary files are available before starting
+    msg = "Checking files..."
+    loglist = addlog(msg,loglist)
 
+    if not os.path.isdir(path+night):
+        msg = "No folder exists for this night. Exiting..."
+        loglist = addlog(msg, loglist)
+        return None
 
-    ## define last image to stack
+    if not os.path.isdir(path+night+"/aligned"):
+        msg = "No 'aligned' folder found, creating 'aligned' folder..."
+        loglist = addlog(msg, loglist)
+        os.mkdir(path+night+"/aligned")
+
+    if not os.path.isfile(pathnam+"data_"+night+".pkl"):
+        msg = "No database called '" + "data_"+night+".pkl" + "' exists. \n " \
+            "Please run initdb() to initialize the database. \n " \
+            "Exiting..."
+        loglist = addlog(msg, loglist)
+        return None
+
+    # load pandas df
+    msg = "Loading database for " + night + "..."
+    loglist = addlog(msg, loglist)
+    dataf = pd.read_pickle(pathnam + 'data_'+night+'.pkl')
+
+    fileID = dataf['fileID']
+    fnames = dataf['filename']
+    msg = "Total science frames:  " + str(len(fnames))
+    loglist = addlog(msg, loglist)
+
+    ## define last image
     if n2 == 'max':
         n2 = len(fnames)
     else:
         n2 = int(n2)
 
-    p = Pool(6)
-    print "shifting images..."
-    p.map(partial(align_rebin, scipathnam = scipathnam,apathnam = apathnam,imshifts = imshifts), fnames[n1:n2],35)
+    # shift & rebin images and save them
+    p = Pool(6) #uses 6 cores
+    msg = "Shifting images..."
+    loglist = addlog(msg, loglist)
+    p.map(partial(align_rebin, scipathnam = scipathnam,apathnam = apathnam,dataf = dataf), fileID[n1:n2],35)
 
-    print "Aligned & rebinned images are saved at " + night + '/aligned with the suffix "_aligned"'
+    msg = "Aligned & rebinned images are saved at " + night + '/aligned with the suffix "_aligned"'
+    loglist = addlog(msg, loglist)
 
-    timefinish(time0)
+
+    #get stop time and save screen output to log
+    msg = timefinish(time0)
+    loglist = addlog(msg, loglist)
+
+    # write/append screen output to logfile
+    msg = "Writing screen output to logfile..."
+    loglist = addlog(msg, loglist)
+    writelog(loglist,night,pathnam)
 
 
-def align_rebin(filename,scipathnam,apathnam,imshifts):
+def align_rebin(fID,scipathnam,apathnam,dataf):
     # print filename
+    filename = dataf.loc[fID,'filename']
     im0 = fits.getdata(scipathnam+filename)
-    im1 = shift(im0, (imshifts.loc[filename]["y"], imshifts.loc[filename]["x"]), mode='constant', cval=0.0)
+    im1 = shift(im0, (dataf.loc[fID,"shift_y"], dataf.loc[fID,"shift_x"]), mode='constant', cval=0.0)
     im2 = rebin(im1, (im1.shape[0] / 4, im1.shape[1] / 4))
     write_to_fits(apathnam + filename.split('.')[0]+'_aligned.' + filename.split('.')[1], im2)  # write to aligned subdir, add suffix "_aligned"
     return
